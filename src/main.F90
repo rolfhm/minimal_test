1453c9fc7fecea3d1f24a193d3ef8af6ec66942c
Rolf Heilemann Myhre <rolfhm@met.no>
2023-02-21 16:20:33 +0100

    minimal compiling, but crashing example

--- src/main.F90
+++ src/main.F90
@@ -1,74 +1,15 @@
-PROGRAM MAIN
+program main
 
-! read command line arguments (nproma, klev, ngpblk)
+  use parkind1, only : jpim
+  use wrapper_mod, only: wrapper
 
-USE PARKIND1, ONLY : JPIM
-USE WRAPPER_MOD
+  implicit none
 
-IMPLICIT NONE
+  integer(kind=jpim) :: nproma,nlev
 
-INTEGER :: IARG
-CHARACTER(LEN=256) :: CARG
-INTEGER(KIND=JPIM) :: NPROMA,NLEV,NGPBLK
-LOGICAL :: LCHECK
+  nproma=32
+  nlev=87
 
-! default arguments
-NPROMA=32
-NLEV=87
-NGPBLK=1
-LCHECK=.FALSE.
+  call wrapper(nproma, nlev)
 
-! parse arguments
-IARG=1
-DO WHILE ( IARG <= COMMAND_ARGUMENT_COUNT() )
-  CALL GET_COMMAND_ARGUMENT(IARG,CARG)
-    SELECT CASE (TRIM(CARG))
-        CASE ('--nproma')
-            IARG=IARG+1
-            CALL GET_COMMAND_ARGUMENT(IARG,CARG)
-            READ(CARG,*) NPROMA
-        CASE ('--nlev')
-            IARG=IARG+1
-            CALL GET_COMMAND_ARGUMENT(IARG,CARG)
-            READ(CARG,*) NLEV
-        CASE ('--ngpblk')
-            IARG=IARG+1
-            CALL GET_COMMAND_ARGUMENT(IARG,CARG)
-            READ(CARG,*) NGPBLK
-        CASE ('--check')
-          LCHECK=.TRUE.
-        CASE ('--help')
-            WRITE (*,*) 'Acraneb2 wrapper'
-            WRITE (*,*)
-            WRITE (*,*) '  acraneb2 is an atmospheric radiation parameterization scheme'
-            WRITE (*,*)
-            WRITE (*,*) '  A scientific description can be found in Geleyn et al., 2017 (doi:10.1002/qj.3006) '
-            WRITE (*,*) '  and Masek et al., 2015 (doi: 10.1002/qj.2653).'
-            WRITE (*,*)
-            WRITE (*,*) '  Data for this wrapper are taken from a 1.3km resolution run with 87 vertical levels '
-            WRITE (*,*) '  with a timestep of 60s, on August 1 2016. Forecast lead time is 12h.'
-            WRITE (*,*) '  Intermittency is switched off.'
-            WRITE (*,*)
-            WRITE (*,*) 'Command line options:'
-            WRITE (*,*) '  --help              show this help'
-            WRITE (*,*) '  --nproma {nproma}   blocking size'
-            WRITE (*,*) '  --nlev   {nlev}     number of vertical levels'
-            WRITE (*,*) '  --ngpblk {ngpblk}   number of blocks'
-            WRITE (*,*) '  --check             check results'
-            STOP
-        CASE DEFAULT
-          WRITE (*,*) 'WARNING:  unknown argument ',TRIM(CARG)
-    END SELECT
-    IARG=IARG+1
-ENDDO
-
-WRITE (*,*) 'Running acraneb2 wrapper with'
-WRITE (*,*) '  nproma = ',NPROMA
-WRITE (*,*) '  nlev   = ',NLEV
-WRITE (*,*) '  ngpblk = ',NGPBLK
-WRITE (*,*) '  lcheck = ',LCHECK
-
-! call wrapper
-CALL WRAPPER(NPROMA,NLEV,NGPBLK,LCHECK)
-
-END PROGRAM MAIN
+end program main
