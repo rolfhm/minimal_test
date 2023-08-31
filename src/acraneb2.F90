27f45278a527bd679cba99d59f1f3fe322a2b8b1
Rolf Heilemann Myhre <rolfhm@met.no>
2023-08-31 13:51:32 +0200

    add non nested

--- src/acraneb2.F90
+++ src/acraneb2.F90
@@ -1,12 +1,17 @@
-subroutine acraneb2(mudobject)
+subroutine acraneb2(miniobject, mudobject)
 
+  use minimod, only: minitype
   use mudmod,  only: mudtype
 
   implicit none
 
-  type(mudtype), intent(inout) :: mudobject
+  type(minitype), intent(in) :: miniobject
+  type(mudtype), intent(in)  :: mudobject
 
   integer :: m, n
+
+  m = miniobject%x
+  n = miniobject%y
 
   m = mudobject%miniobject%x
   n = mudobject%miniobject%y
--- src/wrapper_mod.F90
+++ src/wrapper_mod.F90
@@ -5,12 +5,14 @@ module wrapper_mod
   subroutine wrapper(kgpblk)
 
     use parkind1, only: jpim, jprb
+    use minimod,  only: minitype
     use mudmod,   only: mudtype
 
     implicit none
 
     integer(kind=jpim), intent(in) :: kgpblk
 
+    type(minitype) :: miniobject
     type(mudtype) :: mudobject
 
     integer :: jblk
@@ -18,12 +20,15 @@ module wrapper_mod
 
 #include "acraneb2.intfb.h"
 
+    miniobject%x = 1
+    miniobject%y = 1
+
     mudobject%miniobject%x = 1
     mudobject%miniobject%y = 2
 
     do jblk = 1, kgpblk
 
-      call acraneb2(mudobject)
+      call acraneb2(miniobject, mudobject)
 
     enddo
 
