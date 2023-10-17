e03f057d4032ed8a011e1fc9e0dbe8205dd63767
Rolf Heilemann Myhre <rolfhm@met.no>
2023-10-17 10:11:46 +0200

    maybe minimal

--- src/acraneb_transt.F90
+++ src/acraneb_transt.F90
@@ -1,4 +1,4 @@
-subroutine acraneb_transt(klon, klev, kidia, kfdia, ktdia)
+subroutine acraneb_transt(klon, kidia, kfdia)
 
   use parkind1, only: jpim, jprb
   use xxx, only: blabla, bloblo
@@ -6,25 +6,17 @@ subroutine acraneb_transt(klon, klev, kidia, kfdia, ktdia)
   implicit none
 
   integer(kind=jpim), intent(in) :: klon
-  integer(kind=jpim), intent(in) :: klev
 
   integer(kind=jpim), intent(in) :: kidia
   integer(kind=jpim), intent(in) :: kfdia
-  integer(kind=jpim), intent(in) :: ktdia
 
-  integer(kind=jpim) :: jlon, jlev, i
+  integer(kind=jpim) :: jlon, i
 
   type(blabla) :: lala
 
-  real(kind=jprb) :: za
   real(kind=jprb) :: zq1(klon)
-  real(kind=jprb) :: zq2(klon, klev)
 
   call delta_t(zq1, lala%lolo)
-
-  do jlev = ktdia, klev
-    call delta_t(zq2(1:klon,jlev), lala%lolo)
-  enddo
 
 contains
 
@@ -38,17 +30,11 @@ subroutine delta_t(pq, lolo)
 
   real(kind=jprb) :: za(klon,lolo%lulu)
 
-  real(kind=jprb) :: x, z
-
-  associate(zz => z)
-
   do i = 1,lolo%lulu
     do jlon = 1,klon
       za(jlon,i) = za(jlon,i) + pq(jlon)
     enddo
   enddo
-
-  end associate
 
 end subroutine
 
